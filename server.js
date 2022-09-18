823f3aea91194b8e1f38a5275c48911eba5a3b9c
jaxayM <56200185+jaxayM@users.noreply.github.com>
2022-09-18 06:01:20 +0200

    Update server.js

--- server.js
+++ server.js
@@ -21,3 +21,7 @@ const port = process.env.PORT || 3000
 app.listen(port, () => {
   console.log(`React app listening at http://localhost:${port}`)
 })
+
+app.get("/api", (req, res, next) => {
+  res.status(200).json({ 'you:r':'in' })
+})
